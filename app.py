4cae3a1aa2e99f21875d2867e789a548eedee23d
ramthenmala <devramthenmala@gmail.com>
2023-11-01 23:21:43 +0000

    first post method :-)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 # A SAMPLE PYTHON DATA SET
@@ -14,7 +14,17 @@ stores = [
     }
 ]
 
+# GET STORE
+
 
 @app.get('/store')
 def get_stores():
     return {"stores": stores}
+
+
+@app.post('/store')
+def create_store():
+    request_data = request.get_json()
+    new_store = {'name': request_data['name'], 'items': []}
+    stores.append(new_store)
+    return new_store, 201
